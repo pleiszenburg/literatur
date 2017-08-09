36b8c9de929e6a69d699f356cfa436714554bd03
Sebastian M. Ernst <ernst@pleiszenburg.de>
2017-08-09 13:10:38 +0200

    moved report code

--- scripts/l_report.py
+++ scripts/l_report.py
@@ -7,7 +7,7 @@ LITERATUR
 Literature management with Python, Dropbox and MediaWiki
 https://github.com/pleiszenburg/literatur
 
-	scripts/run_report.py: Produces reports for emails etc
+	scripts/l_report.py: Produces reports for emails etc
 
 	Copyright (C) 2017 Sebastian M. Ernst <ernst@pleiszenburg.de>
 
@@ -30,56 +30,13 @@ specific language governing rights and limitations under the License.
 # IMPORTS
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-from lw_strings import *
-from lw_groups import *
-from lw_file import *
-from lw_storage import *
-from lw_index import *
-from lw_report import *
-
-import pprint
+from literatur.index import report
 
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# LOAD
+# REPORT
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-lit_working_path = lit_path_local # TODO read path from config
+if __name__ == "__main__":
 
-# Load base index
-lit_list_full_base = lit_read_pickle(lit_working_path + lit_path_subfolder_db + lit_path_pickle_base)
-
-# Load old index
-lit_list_full_old = lit_read_pickle(lit_working_path + lit_path_subfolder_db + lit_path_pickle_old)
-
-# Load new index
-lit_list_full_new = lit_read_pickle(lit_working_path + lit_path_subfolder_db + lit_path_pickle_new)
-
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# DIFF, REPORT
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-# Diff base vs new index
-diff_base_rm, diff_base_new, diff_base_mv, diff_base_changed = lit_diff_lists(lit_list_full_base, lit_list_full_new)
-
-# Diff old vs new index
-diff_old_rm, diff_old_new, diff_old_mv, diff_old_changed = lit_diff_lists(lit_list_full_old, lit_list_full_new)
-
-# Store diffs
-lit_write_pprint(diff_old_rm, lit_working_path + lit_path_subfolder_db + lit_path_report_old_pprint_rm)
-lit_write_pprint(diff_old_new, lit_working_path + lit_path_subfolder_db + lit_path_report_old_pprint_new)
-lit_write_pprint(diff_old_mv, lit_working_path + lit_path_subfolder_db + lit_path_report_old_pprint_mv)
-lit_write_pprint(diff_old_changed, lit_working_path + lit_path_subfolder_db + lit_path_report_old_pprint_changed)
-lit_write_pprint(diff_base_rm, lit_working_path + lit_path_subfolder_db + lit_path_report_base_pprint_rm)
-lit_write_pprint(diff_base_new, lit_working_path + lit_path_subfolder_db + lit_path_report_base_pprint_new)
-lit_write_pprint(diff_base_mv, lit_working_path + lit_path_subfolder_db + lit_path_report_base_pprint_mv)
-lit_write_pprint(diff_base_changed, lit_working_path + lit_path_subfolder_db + lit_path_report_base_pprint_changed)
-
-# Generate email text with new entries
-diff_old_mail_new = report_mail_newfiles(diff_old_new)
-diff_base_mail_new = report_mail_newfiles(diff_base_new)
-
-# Store mail reports
-lit_write_plaintext(diff_old_mail_new, lit_working_path + lit_path_subfolder_db + lit_path_report_old_mail_new)
-lit_write_plaintext(diff_base_mail_new, lit_working_path + lit_path_subfolder_db + lit_path_report_base_mail_new)
+	report()
--- src/literatur/index.py
+++ src/literatur/index.py
@@ -142,3 +142,39 @@ def rebuild_index():
 		list_full_new,
 		os.path.join(PATH_ROOT, PATH_SUB_DB, FILE_DB_CURRENT)
 		)
+
+
+def report():
+
+	# Load base index
+	lit_list_full_base = lit_read_pickle(lit_working_path + lit_path_subfolder_db + lit_path_pickle_base)
+
+	# Load old index
+	lit_list_full_old = lit_read_pickle(lit_working_path + lit_path_subfolder_db + lit_path_pickle_old)
+
+	# Load new index
+	lit_list_full_new = lit_read_pickle(lit_working_path + lit_path_subfolder_db + lit_path_pickle_new)
+
+	# Diff base vs new index
+	diff_base_rm, diff_base_new, diff_base_mv, diff_base_changed = lit_diff_lists(lit_list_full_base, lit_list_full_new)
+
+	# Diff old vs new index
+	diff_old_rm, diff_old_new, diff_old_mv, diff_old_changed = lit_diff_lists(lit_list_full_old, lit_list_full_new)
+
+	# Store diffs
+	lit_write_pprint(diff_old_rm, lit_working_path + lit_path_subfolder_db + lit_path_report_old_pprint_rm)
+	lit_write_pprint(diff_old_new, lit_working_path + lit_path_subfolder_db + lit_path_report_old_pprint_new)
+	lit_write_pprint(diff_old_mv, lit_working_path + lit_path_subfolder_db + lit_path_report_old_pprint_mv)
+	lit_write_pprint(diff_old_changed, lit_working_path + lit_path_subfolder_db + lit_path_report_old_pprint_changed)
+	lit_write_pprint(diff_base_rm, lit_working_path + lit_path_subfolder_db + lit_path_report_base_pprint_rm)
+	lit_write_pprint(diff_base_new, lit_working_path + lit_path_subfolder_db + lit_path_report_base_pprint_new)
+	lit_write_pprint(diff_base_mv, lit_working_path + lit_path_subfolder_db + lit_path_report_base_pprint_mv)
+	lit_write_pprint(diff_base_changed, lit_working_path + lit_path_subfolder_db + lit_path_report_base_pprint_changed)
+
+	# Generate email text with new entries
+	diff_old_mail_new = report_mail_newfiles(diff_old_new)
+	diff_base_mail_new = report_mail_newfiles(diff_base_new)
+
+	# Store mail reports
+	lit_write_plaintext(diff_old_mail_new, lit_working_path + lit_path_subfolder_db + lit_path_report_old_mail_new)
+	lit_write_plaintext(diff_base_mail_new, lit_working_path + lit_path_subfolder_db + lit_path_report_base_mail_new)
