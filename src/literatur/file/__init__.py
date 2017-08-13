a4a6984c6dba4a2b9ff55cfabcbb166b1af60825
Sebastian M. Ernst <ernst@pleiszenburg.de>
2017-08-13 14:52:36 +0200

    magic for file type identification

---
+++ src/literatur/file/__init__.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+LITERATUR
+Literature management with Python, Dropbox and MediaWiki
+https://github.com/pleiszenburg/literatur
+
+	src/literatur/file/__init__.py: handle individual files
+
+	Copyright (C) 2017 Sebastian M. Ernst <ernst@pleiszenburg.de>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU Lesser General Public License
+Version 2.1 ("LGPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+https://github.com/pleiszenburg/literatur/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORTS
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+import magic
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ROUTINES
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+def get_file_type(filename):
+
+	# Get info from magic
+	magic_info = magic.from_file(filename)
+
+	# This will contain a unique file type string, by default empty
+	file_type = ''
+
+	# Identify a pdf document
+	if value.startswith('PDF document'):
+		file_type = 'pdf'
+
+	return file_type, magic_info
+
+
+def get_file_mimetype(filename):
+
+	return magic.from_file(filename, mime = True)
