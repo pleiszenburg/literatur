b560651f9b65847a27a76923c82f41c395c857d6
Sebastian M. Ernst <ernst@pleiszenburg.de>
2017-08-18 10:23:52 +0200

    type info api cleanup

--- src/literatur/filetypes/__typeinfo__.py
+++ src/literatur/filetypes/__typeinfo__.py
@@ -6,7 +6,7 @@ LITERATUR
 Literature management with Python, Dropbox and MediaWiki
 https://github.com/pleiszenburg/literatur
 
-	src/literatur/file/typeinfo.py: indentify file types
+	src/literatur/filetypes/__typeinfo__.py: indentify file types
 
 	Copyright (C) 2017 Sebastian M. Ernst <ernst@pleiszenburg.de>
 
@@ -31,26 +31,40 @@ specific language governing rights and limitations under the License.
 
 import magic
 
-from ..filetypes import filetypes
+from .__loader__ import get_file_types as __get_file_types__
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# LOADER ROUTINE
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+filetypes = __get_file_types__()
 
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # ROUTINES
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-def get_file_type(filename):
+def get_literatur_type_from_filename(filename):
 
-	# Get info from magic
-	magic_info = magic.from_file(filename)
+	return get_literatur_type_from_magicinfo(get_magicinfo(filename))
+
+
+def get_literatur_type_from_magicinfo(magic_info):
 
 	# Test magic info through all plugins, return true if one reports a match
 	for filetype in filetypes.keys():
 		if filetypes[filetype].test_magic_info(magic_info):
-			return filetype, magic_info
+			return filetype
 
-	return None, magic_info
+	return None
 
 
-def get_file_mimetype(filename):
+def get_magicinfo(filename):
+
+	return magic.from_file(filename)
+
+
+def get_mimetype(filename):
 
 	return magic.from_file(filename, mime = True)
