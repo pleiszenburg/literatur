0e2f2851ce725accc17458040ce58987e7e7e474
Sebastian M. Ernst <ernst@pleiszenburg.de>
2017-09-11 14:47:59 +0200

    fixed key names

--- src/literatur/parser/userinput.py
+++ src/literatur/parser/userinput.py
@@ -28,21 +28,29 @@ specific language governing rights and limitations under the License.
 # IMPORT
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+from .string import clean_str
+
 from ..const import (
-	KEY_ANNOTATION
-	KEY_AUTHOR_FIRST
-	KEY_AUTHORS_DICT
-	KEY_AUTHORS_LIST
-	KEY_CLASS
-	KEY_EDITORS_DICT
-	KEY_EDITORS_LIST
-	KEY_ETAL_BOOL
-	KEY_EXTENSION
-	KEY_SERIES_ID
-	KEY_SERIES_NAME
-	KEY_SERIES_SECTION
-	KEY_SERIES_TYPE
-	KEY_TITLE
+	DEFAULT_CLASS,
+	DEFAULT_YEAR,
+	DEFAULT_YEAR_MAX,
+	DEFAULT_YEAR_MIN,
+	DELIMITER_USERINPUT_BLOCK,
+	DELIMITER_USERINPUT_SERIES,
+	KEY_ANNOTATION,
+	KEY_AUTHOR_FIRST,
+	KEY_AUTHORS_DICT,
+	KEY_AUTHORS_LIST,
+	KEY_CLASS,
+	KEY_EDITORS_DICT,
+	KEY_EDITORS_LIST,
+	KEY_ETAL_BOOL,
+	KEY_EXTENSION,
+	KEY_SERIES_ID,
+	KEY_SERIES_NAME,
+	KEY_SERIES_SECTION,
+	KEY_SERIES_TYPE,
+	KEY_TITLE,
 	KEY_YEAR
 	)
 
@@ -53,35 +61,34 @@ from ..const import (
 
 def userinput_str_to_metaentry_dict(in_str):
 
-	fragments_list = in_str.split(input_delimiter)
+	fragments_list = in_str.split(DELIMITER_USERINPUT_BLOCK)
 
 	# Step 1: Class
-	item_class = lit_class_default
+	item_class = DEFAULT_CLASS
 	if len(fragments_list) > 0:
-		item_class = clean_string(fragments_list[0]).upper().replace(" ", "-")
-		# Only known classes, otherwise default class
+		item_class = clean_str(in_str).upper().replace(' ', '-')
 		if item_class not in lit_classes:
-			item_class = lit_class_default
+			item_class = DEFAULT_CLASS
 
 	# Step 2: Year
-	item_year = lit_year_default
+	item_year = str(DEFAULT_YEAR)
 	item_bookid = ''
 	item_book = ''
 	item_editors = ''
 	item_type = ''
 	item_section = ''
 	if len(fragments_list) > 1:
-		item_year_f = clean_string(fragments_list[1])
+		item_year_f = clean_str(fragments_list[1])
 		if len(item_year_f) > 3:
 			# Is year a number? Go back to default if not
 			if item_year_f[:4].isdigit():
 				item_year = item_year_f[:4]
 				# Check for unrealistic years
-				if int(item_year) > 2100 or int(item_year) < 1000:
-					item_year = lit_year_default
-				# Fetch books etc
+				if item_year > DEFAULT_YEAR_MAX or int(item_year) < DEFAULT_YEAR_MIN:
+					item_year = str(DEFAULT_YEAR)
+				# Fetch series etc
 				if len(item_year_f) > 4:
-					item_book_s = item_year_f[4:].split(input_delimiter2)
+					item_book_s = item_year_f[4:].split(DELIMITER_USERINPUT_SERIES)
 					item_bookid = item_book_s[0].strip(' ').replace(" ", "-")
 					item_book, item_editors, item_type = lit_get_book_from_bookid(item_year, item_bookid)
 					if len(item_book_s) > 1:
@@ -90,7 +97,7 @@ def userinput_str_to_metaentry_dict(in_str):
 	# Step 3: Authors
 	item_authors_d = lit_authors_default
 	if len(fragments_list) > 2:
-		item_authors_d = clean_string(fragments_list[2])
+		item_authors_d = clean_str(fragments_list[2])
 		if len(item_authors_d) == 0:
 			item_authors_d = lit_authors_default
 	# Generate author dictionary
@@ -99,14 +106,14 @@ def userinput_str_to_metaentry_dict(in_str):
 	# Step 4: Title
 	item_title_d = lit_title_default
 	if len(fragments_list) > 3:
-		item_title_d = clean_string(fragments_list[3])
+		item_title_d = clean_str(fragments_list[3])
 		if len(item_title_d) == 0:
 			item_title_d = lit_title_default
 
 	# Step 5: Annotation
 	item_ann_d = lit_ann_default
 	if len(fragments_list) > 4:
-		item_ann_d = clean_string(fragments_list[4])
+		item_ann_d = clean_str(fragments_list[4])
 		if len(item_ann_d) == 0:
 			item_ann_d = lit_ann_default
 
@@ -114,7 +121,7 @@ def userinput_str_to_metaentry_dict(in_str):
 	item_fileformat = lit_fileformat_default
 	if len(fragments_list) > 5:
 		item_fileformat = fragments_list[5].replace(".", " ")
-		item_fileformat = clean_string(item_fileformat).replace(" ", ".").lower()
+		item_fileformat = clean_str(item_fileformat).replace(" ", ".").lower()
 		if item_fileformat not in list_ext:
 			item_fileformat = lit_fileformat_default
 
